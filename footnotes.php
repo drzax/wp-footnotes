aed097d4a5b36d9fc0758dee2ba2930eca3ac480
Christian Wach <needle@haystack.co.uk>
2012-10-23 11:03:39 +0100

    more specific options page name, as people often have more than one footnotes plugin

--- footnotes.php
+++ footnotes.php
@@ -270,7 +270,7 @@ class swas_wp_footnotes {
 	 */
 	function add_options_page() {
 		// Add a new menu under Options:
-		add_options_page('Footnotes', 'Footnotes', 'manage_options', __FILE__, array($this, 'footnotes_options_page'));
+		add_options_page('WP Footnotes', 'WP Footnotes', 'manage_options', __FILE__, array($this, 'footnotes_options_page'));
 	}
 	
 	
