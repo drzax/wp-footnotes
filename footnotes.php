0aae9ce3ca47ad9cc1317c2094ab715ac0342816
Chris Hastie <chris@oak-wood.co.uk>
2013-03-13 19:59:41 +0000

    Make pretty tooltips optional

--- footnotes.php
+++ footnotes.php
@@ -35,7 +35,7 @@ class swas_wp_footnotes {
 	private $current_options;
 	private $default_options;
 	
-	const OPTIONS_VERSION = "4.2.6"; // Incremented when the options array changes.
+	const OPTIONS_VERSION = "4.2.7"; // Incremented when the options array changes.
 	
 	/**
 	 * Constructor.
@@ -77,6 +77,7 @@ class swas_wp_footnotes {
 									  'footnotes_open'=>' ((',
 									  'footnotes_close'=>'))',
 									  'footnotes_shortcode'=>'footnotes',
+									  'pretty_tooltips'=>false,
 									  'version'=>self::OPTIONS_VERSION);
 		
 		// Get the current settings or setup some defaults if needed
@@ -127,6 +128,8 @@ class swas_wp_footnotes {
 			$footnotes_options['footnotes_open'] = $_POST['footnotes_open'];
 			$footnotes_options['footnotes_close'] = $_POST['footnotes_close'];
 			
+			$footnotes_options['pretty_tooltips'] = (array_key_exists('pretty_tooltips', $_POST)) ? true : false;
+			
 			update_option('swas_footnote_options', $footnotes_options);
 		} elseif( !empty($_POST['reset_options']) ) {
 			update_option('swas_footnote_options', '');
@@ -138,7 +141,7 @@ class swas_wp_footnotes {
 		add_action('admin_menu', array($this, 'add_options_page')); 		// Insert the Admin panel.
 		add_action('admin_enqueue_scripts', array($this, 'register_js'));
 		add_action('wp_head', array($this, 'insert_styles'));
-		add_action('wp_enqueue_scripts', array($this, 'tooltip_scripts'));
+		if ($this->current_options['pretty_tooltips']) add_action('wp_enqueue_scripts', array($this, 'tooltip_scripts'));
 	}
 	
 	/**
