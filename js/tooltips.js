drzax/wp-footnotes

commit 022dbb580d55d4610c702327defc252803875444

Add a Grunt build process into the mix for minified JS & CSS

js/tooltips.js
@@ -1,29 +1,1 @@
-jQuery(document).ready(function() {
-  // hack to get tooltip to persist whilst it is hovered over
-  jQuery( "a.footnote-identifier-link" ).bind( 
-    "mouseleave", 
-    function( event ) { 
-      event.stopImmediatePropagation(); 
-      var fixed = setTimeout('jQuery("a.footnote-identifier-link").tooltip("close")', 500); 
-      jQuery(".ui-tooltip").hover( 
-        function(){
-          clearTimeout (fixed);
-        }, 
-        function(){
-          jQuery("a.footnote-identifier-link").tooltip("close");
-        }
-      ); 
-    }
-  ).tooltip();
-  
-  
-  jQuery( "a.footnote-identifier-link" ).tooltip({
-    items: "a.footnote-identifier-link",
-    tooltipClass: "footnote footnote-tooltip",
-    content: function() {
-      var element = jQuery( this );
-      var bibentry = element.attr("href");      
-      return jQuery(bibentry).html();
-    }
-  });
-});
+(function(t){t(function(){t("a.footnote-identifier-link").bind("mouseleave",function(o){o.stopImmediatePropagation();var i=setTimeout('$("a.footnote-identifier-link").tooltip("close")',500);t(".ui-tooltip").hover(function(){clearTimeout(i)},function(){t("a.footnote-identifier-link").tooltip("close")})}).tooltip(),t("a.footnote-identifier-link").tooltip({items:"a.footnote-identifier-link",tooltipClass:"footnote footnote-tooltip",content:function(){var o=t(this),i=o.attr("href");return t(i).html()}})})})(jQuery);

src/js/tooltips.js
@@ -1,0 +1,32 @@
+;(function($, undefined){
+	$(function() {
+		
+		// hack to get tooltip to persist whilst it is hovered over
+		$( "a.footnote-identifier-link" ).bind(
+			"mouseleave", 
+			function( event ) { 
+				event.stopImmediatePropagation(); 
+				var fixed = setTimeout('$("a.footnote-identifier-link").tooltip("close")', 500); 
+				$(".ui-tooltip").hover( 
+					function(){
+						clearTimeout (fixed);
+					}, 
+					function(){
+						$("a.footnote-identifier-link").tooltip("close");
+					}
+				); 
+			}
+		).tooltip();
+
+
+		$( "a.footnote-identifier-link" ).tooltip({
+			items: "a.footnote-identifier-link",
+			tooltipClass: "footnote footnote-tooltip",
+			content: function() {
+				var element = $( this );
+				var bibentry = element.attr("href");
+				return $(bibentry).html();
+			}
+		});
+	}); 
+}(jQuery));

.gitignore
@@ -1,2 +1,4 @@
+.sass-cache
 .svn
 .htaccess
+node_modules

Gruntfile.js
@@ -1,0 +1,44 @@
+/*
+ * Setup options for grunt to work with.
+ */
+module.exports = function(grunt) {
+	// Project configuration.
+	grunt.initConfig({
+		uglify: {
+			production: {
+				options: {
+					preserveComments: 'some'
+				},
+				files: {
+					'js/tooltips.js': ['src/js/tooltips.js'],
+					'js/admin.js': ['src/js/admin.js']
+				}
+			}
+		},
+		compass: {
+			production: {
+				options: {
+					sassDir: 'src/scss',
+					cssDir: 'css',
+					environment: 'production'
+				}
+			}
+		},
+		watch: {
+			scripts: {
+				files: ['src/js/*.js'],
+				tasks: 'uglify'
+			},
+			styles: {
+				files: ['src/scss/*.scss'],
+				tasks: 'compass'
+			}
+		}
+	});
+
+	// Load the plugin that provides the "uglify" task.
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+};

css/admin.css
@@ -1,21 +1,1 @@
-#wp-footnotes-identifier-options,
-#wp-footnotes-backlink-options {
-	border:none; 
-	line-height:20px; 
-	margin-bottom:9px; 
-	padding:10px; 
-	background:#EAF3FA; 
-	-moz-border-radius:5px; 
-	-webkit-border-radius: 5px; 
-	border-radius: 5px;
-}
-
-.identifier-link label { 
-	display: block;
-	border-bottom: 1px solid #21759B;
-}
-
-.form-table ul.checkboxes {
-	list-style-type: none;
-	margin: 0;
-}
+#wp-footnotes-identifier-options,#wp-footnotes-backlink-options{border:none;line-height:20px;margin-bottom:9px;padding:10px;background:#EAF3FA;-moz-border-radius:5px;-webkit-border-radius:5px;border-radius:5px}.identifier-link label{display:block;border-bottom:1px solid #21759b}.form-table ul.checkboxes{list-style-type:none;margin:0}

css/tooltips.css
@@ -1,34 +1,1 @@
-.ui-tooltip {
-  padding: 8px;
-  position: absolute;
-  z-index: 9999;
-  max-width: 300px;
-  -webkit-box-shadow: 0 0 5px #ddd;
-  box-shadow: 0 0 5px #ddd;
-}
-
-.ui-widget {
-  /*font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;*/
-  font-size: 1em;
-  font-size: 1rem;
-  line-height: 1.4;
-}
-
-/* Round corners */
-.ui-corner-all {
-  border-top-left-radius: 4px;
-  border-top-right-radius: 4px;
-  border-bottom-left-radius: 4px;
-  border-bottom-right-radius: 4px;
-}
-
-.ui-widget-content {
-  border: 1px solid #dddddd;
-  background: #eeeeee;
-  color: #333333;
-}
-
-/* hide the back links in the tooltips */
-.footnote-tooltip .footnote-back-link-wrapper {
-  display: none;
-}
+.ui-tooltip{padding:8px;position:absolute;z-index:9999;max-width:300px;-webkit-box-shadow:0 0 5px #ddd;-moz-box-shadow:0 0 5px #ddd;box-shadow:0 0 5px #ddd}.ui-widget{font-size:1em;font-size:1rem;line-height:1.2}.ui-corner-all{-webkit-border-radius:4px;-moz-border-radius:4px;-ms-border-radius:4px;-o-border-radius:4px;border-radius:4px}.ui-widget-content{border:1px solid #ddd;background:#eeeeee;color:#333333}.footnote-tooltip .footnote-back-link-wrapper{display:none}

js/admin.js
@@ -1,0 +1,5 @@
+/*!
+ * This file is part of WP-Footnotes a plugin for WordPress
+ * Copyright (C) 2007-2013 Simon Elvery
+ */
+(function(t){function e(){var e,o="<strong>Example:</strong> A sentence with a footnote",n=t("#list_style_type").val();"decimal"===n&&(e="4"),"decimal-leading-zero"===n&&(e="04"),"lower-alpha"===n&&(e="d"),"upper-alpha"===n&&(e="D"),"lower-roman"===n&&(e="iv"),"upper-roman"===n&&(e="IV"),"symbol"===n&&(e=t("#list_style_symbol").val()),i=i||t('<div id="wp-footnotes-identifier-example">').appendTo("#wp-footnotes-identifier-options"),t("#superscript").is(":checked")&&(o+="<sup>"),o+=t("#pre_identifier").val()+'<a href="javascript:;">',o+=t("#inner_pre_identifier").val(),o+=e,o+=t("#inner_post_identifier").val()+"</a>",o+=t("#post_identifier").val(),t("#superscript").is(":checked")&&(o+="</sup>"),i.html(o+".")}var i;t(function(){t("#list_style_type").change(function(){"symbol"===t(this).val()?t("#list_style_symbol_container").slideDown():t("#list_style_symbol_container").slideUp()}).change(),t("#pre_identifier,#inner_pre_identifier,#inner_post_identifier,#post_identifier,#list_style_symbol").on("keyup",e),t("#list_style_type").on("change",e),t("#superscript").on("click change",e),e(),t(".toggle-section").click(function(){var e=t(this);return t(e.attr("href")).slideToggle(),e.text("show"===e.text()?"hide":"show"),!1})})})(jQuery);

src/js/admin.js
@@ -18,7 +18,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-(function($, undefined){
+;(function($, undefined){
 	
 	var exampleContainer;
 

commit 3affb85a85b18d625b4b4dd9169e369d45266678

Bug fixes for tooltips.js
- Fix regression introduced in 022dbb5
- Fix bug where multiple quick succession mouseovers on identifier links would result in tooltips not displaying.

js/tooltips.js
@@ -1,1 +1,1 @@
-(function(t){t(function(){t("a.footnote-identifier-link").bind("mouseleave",function(o){o.stopImmediatePropagation();var i=setTimeout('$("a.footnote-identifier-link").tooltip("close")',500);t(".ui-tooltip").hover(function(){clearTimeout(i)},function(){t("a.footnote-identifier-link").tooltip("close")})}).tooltip(),t("a.footnote-identifier-link").tooltip({items:"a.footnote-identifier-link",tooltipClass:"footnote footnote-tooltip",content:function(){var o=t(this),i=o.attr("href");return t(i).html()}})})})(jQuery);
+(function(o){var t,e,n=300,i="a.footnote-identifier-link";o(function(){e=o(i),e.tooltip({tooltipClass:"footnote footnote-tooltip",content:function(){var t=o(this),e=t.attr("href");return o(e).html()}}).on("mouseleave focusout",function(o){o.stopImmediatePropagation(),t=setTimeout(function(){e.tooltip("close")},n)}).on("mouseover focusin",function(){clearTimeout(t)}),o(document).on("mouseover",".ui-tooltip",function(){clearTimeout(t)}),o(document).on("mouseleave",".ui-tooltip",function(){clearTimeout(t),t=setTimeout(function(){e.tooltip("close")},n)})})})(jQuery);

src/js/tooltips.js
@@ -1,32 +1,42 @@
 ;(function($, undefined){
+	
+	var timeout,
+		timing = 300,
+		$identifiers,
+		selector = 'a.footnote-identifier-link';
+	
 	$(function() {
 		
-		// hack to get tooltip to persist whilst it is hovered over
-		$( "a.footnote-identifier-link" ).bind(
-			"mouseleave", 
-			function( event ) { 
-				event.stopImmediatePropagation(); 
-				var fixed = setTimeout('$("a.footnote-identifier-link").tooltip("close")', 500); 
-				$(".ui-tooltip").hover( 
-					function(){
-						clearTimeout (fixed);
-					}, 
-					function(){
-						$("a.footnote-identifier-link").tooltip("close");
-					}
-				); 
-			}
-		).tooltip();
-
-
-		$( "a.footnote-identifier-link" ).tooltip({
-			items: "a.footnote-identifier-link",
+		$identifiers = $(selector);
+		
+		$identifiers.tooltip({
 			tooltipClass: "footnote footnote-tooltip",
 			content: function() {
 				var element = $( this );
 				var bibentry = element.attr("href");
 				return $(bibentry).html();
 			}
+		}).on('mouseleave focusout', function(event){
+			
+			// Stop jQuery UI tooltips widget getting the signal to close the tooltip.
+			// Note: this is problematic as it may interfere with other JS acting on these events.
+			event.stopImmediatePropagation();
+			timeout = setTimeout(function() {$identifiers.tooltip("close")}, timing); 
+			
+		}).on('mouseover focusin', function(event){
+			clearTimeout(timeout);
 		});
-	}); 
+		
+		// Stop the tooltip from closing while mouse is hovered.
+		$(document).on('mouseover', '.ui-tooltip', function(){
+			clearTimeout(timeout);
+		});
+		
+		// Close the tooltip when mouse leaves tooltip.
+		$(document).on('mouseleave', '.ui-tooltip', function(){
+			clearTimeout(timeout);
+			timeout = setTimeout(function() {$identifiers.tooltip("close")}, timing); 
+		});
+	});
+	
 }(jQuery));