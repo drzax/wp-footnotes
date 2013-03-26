f5f1c5fcbe521a1287a549f9a4a96747f13151f3
Simon Elvery <simon@elvery.net>
2013-03-27 09:57:56 +1000

    Make sure plugin is activated as first test

--- tests/test-footnotes.php
+++ tests/test-footnotes.php
@@ -1,17 +1,12 @@
 <?php
 
-class FootnotesTest extends WP_UnitTestCase {
+class WP_Test_Footnotes extends WP_UnitTestCase {
 
-	function testSample() {
-		// replace this with some actual testing code
-		$this->assertTrue( true );
+	/**
+	 * Make sure plugin is activated
+	 */
+	function test_activated() {
+		$this->assertTrue( class_exists( 'swas_wp_footnotes' ), 'swas_wp_footnotes class not defined' );
 	}
 	
-	function testAdminStylesRegistered() {
-		$this->assertTrue( wp_style_is( 'wp-footnotes-admin-styles', 'registered' ) );
-	}
-	
-	function testAdminScriptsRegistered() {
-		$this->assertTrue( wp_script_is( 'wp-footnotes-admin', 'registered' ) );
-	}
 }
