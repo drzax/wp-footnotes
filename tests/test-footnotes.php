08104d3d2394a53be551d455814a5c9f34c51ca4
Simon Elvery <simon@elvery.net>
2013-03-26 21:54:25 +1000

    Test admin styles are being added

--- tests/test-footnotes.php
+++ tests/test-footnotes.php
@@ -6,4 +6,8 @@ class FootnotesTest extends WP_UnitTestCase {
 		// replace this with some actual testing code
 		$this->assertTrue( true );
 	}
+	
+	function testAdminStylesRegistered() {
+		$this->assertTrue( wp_style_is( 'wp-footnotes-admin-styles', 'registered' ) );
+	}
 }
