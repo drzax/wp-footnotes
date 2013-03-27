1f18c6adb92bb503539fb0c6a5b342cd163318e5
Simon Elvery <simon@elvery.net>
2013-03-27 21:25:08 +1000

    Test that the content filter is added

--- tests/test-footnotes.php
+++ tests/test-footnotes.php
@@ -9,4 +9,8 @@ class WP_Test_Footnotes extends WP_UnitTestCase {
 		$this->assertTrue( class_exists( 'swas_wp_footnotes' ), 'swas_wp_footnotes class not defined' );
 	}
 	
+	function test_post_filter_added() {
+		$this->assertEquals( 11, has_filter('the_content', array($swas_wp_footnotes, 'process')) );
+	}
+	
 }
